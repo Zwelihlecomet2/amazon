9ec64571fee4dd948ce16bcd1ea0e8027d600f52
zwelihle comet <zwelihlecomet2@gmail.com>
2025-08-26 09:55:28 +0100

    Final deployment and firebase backend hosting

--- src/Axios.js
+++ src/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: "http://127.0.0.1:5001/zaio-2c173/us-central1/api"
+    baseURL: "https://us-central1-zaio-2c173.cloudfunctions.net/api"
 });
 
 export default instance;
